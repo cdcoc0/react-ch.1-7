604e159d81ad0765f6bdd1ca2378e13f960ee88d
velykitty <jiyunee13@gmail.com>
2021-01-09 17:34:53 +0900

    JSX -스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 function App() {
   const name = undefined;
+  const style = {
+    //카멜 표기법 사용
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 //default: px
+  }
+
   return (
-    <>
+    <div style={style}>
       {name || 'hello react!😉'}
-    </>
+    </div>
+  //   <div style={{
+    // backgroundColor: 'black',
+    // color: 'aqua',
+    // fontSize: '48px',
+    // fontWeight: 'bold',
+    // padding: 16
+  //}}>
+  //   {name || 'hello react!😉'}
+  // </div>
   );
 }
 
