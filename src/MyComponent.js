9db8082d555e3306afd74b46291a748aa9b780e1
velykitty <jiyunee13@gmail.com>
2021-01-12 13:04:38 +0900

    props -클래스형 컴포넌트에서 props 사용

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({name, favoriteNumber, children}) => {
-    return (
-        <div>
-            My name is {name}😀 <br />
-            children: {children} <br />
-            My favorite number is {favoriteNumber}
-        </div>
-    );
-};
+class MyComponent extends Component {
+    render() {
+        const {name, favoriteNumber, children} = this.props;
+        return (
+            <div>Hey {name}😁! <br />
+            There is a {favoriteNumber}-dollar bill right in front of you! <br />
+            children: {children}
+            </div>
+        );
+    }
+}
 
 MyComponent.defaultProps = {
-    name: 'default'
+    name: 'default name'
 };
 
 MyComponent.propTypes = {
     name: PropTypes.string,
     favoriteNumber: PropTypes.number.isRequired
-};
+}
 
 export default MyComponent;
+
+// const MyComponent = ({name, favoriteNumber, children}) => {
+//     return (
+//         <div>
+//             My name is {name}😀 <br />
+//             children: {children} <br />
+//             My favorite number is {favoriteNumber}
+//         </div>
+//     );
+// };
+
+// MyComponent.defaultProps = {
+//     name: 'default'
+// };
+
+// MyComponent.propTypes = {
+//     name: PropTypes.string,
+//     favoriteNumber: PropTypes.number.isRequired
+// };
+
+// export default MyComponent;
