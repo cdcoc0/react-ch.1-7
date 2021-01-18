d8b3ebaec9ad6d89cf594847b942420e4d9d63f1
velykitty <jiyunee13@gmail.com>
2021-01-18 18:09:30 +0900

    props -클래스형 컴포넌트, class 내부에서 prop설정

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+    static defaultProps = {
+        name: 'default name'
+    };
+
+    static propTypes = {
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired
+    }
     render() {
         const {name, favoriteNumber, children} = this.props;
         return (
@@ -13,14 +21,14 @@ class MyComponent extends Component {
     }
 }
 
-MyComponent.defaultProps = {
-    name: 'default name'
-};
+// MyComponent.defaultProps = {
+//     name: 'default name'
+// };
 
-MyComponent.propTypes = {
-    name: PropTypes.string,
-    favoriteNumber: PropTypes.number.isRequired
-}
+// MyComponent.propTypes = {
+//     name: PropTypes.string,
+//     favoriteNumber: PropTypes.number.isRequired
+// }
 
 export default MyComponent;
 
