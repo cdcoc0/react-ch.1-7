34d4e039c0947854b0f23a1b23face113c6c31a9
velykitty <jiyunee13@gmail.com>
2021-01-19 18:43:03 +0900

    state -constructor 메서드 없이 state 초기값 설정

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        //state의 초깃값 설정
-        this.state = {
-            number: 0
-        };
+    //     //state의 초깃값 설정
+    //     this.state = {
+    //         number: 0,
+    //         fixedNumber: 0
+    //     };
+    // }
+    state = {
+        number: 0,
+        fixedNumber: 0
     }
 
     render() {
-        const {number} = this.state;
+        const {number, fixedNumber} = this.state;
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
                 //onClick 함수 지정
                 onClick={() => {
