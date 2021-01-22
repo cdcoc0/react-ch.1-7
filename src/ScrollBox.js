b46a8fe41f9607275587eb457e955d2284dff7fa
velykitty <jiyunee13@gmail.com>
2021-01-22 17:38:47 +0900

    ref -컴포넌트에 메서드 생성 : 스크롤바를 맨 아래쪽으로 내리는 메서드 작성

--- src/ScrollBox.js
+++ src/ScrollBox.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 
 class ScrollBox extends Component {
+
+    scrollToBottom = () => {
+        const {scrollHeight, clientHeight} = this.box;
+        //const scrollHeight = this.box.scrollHeight;
+        //const clientHeight = this.box.clientHeight;
+        this.box.scrollTop = scrollHeight - clientHeight;
+    }
+
     render() {
         const style = {
             border: '1px solid black',
