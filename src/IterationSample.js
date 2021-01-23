c2e842ce840ee501111ef59cbc55439b89e0d39f
velykitty <jiyunee13@gmail.com>
2021-01-23 16:29:04 +0900

    component iteration -key 설정 : index는 배열이 변경될 때 효율적으로 리렌더링하지 못하기 때문에 index 값은 고유한 값이 없을 때만 사용한다.

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -3,7 +3,7 @@ import React from 'react';
 const IterationSample = () => {
 
     const names = ['snowman', 'ice', 'snow', 'wind'];
-    const nameList = names.map(name => <li>{name}</li>)
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>)
     return(
         <ul>
             {nameList}
