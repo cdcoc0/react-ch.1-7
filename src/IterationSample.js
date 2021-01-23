1165d518014d848c72678bbe9ff48c049084596c
velykitty <jiyunee13@gmail.com>
2021-01-23 16:38:55 +0900

    compenent iteration -set initial status : key와 text값의 객체 배열 사용해 리스트 생성

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IterationSample = () => {
 
-    const names = ['snowman', 'ice', 'snow', 'wind'];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>)
+    const [names, setNames] = useState([
+        {id: 0, text: 'snowman'},
+        {id: 1, text: 'ice'},
+        {id: 2, text: 'snow'},
+        {id: 3, text: 'wind'}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(4);
+
+    const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+
     return(
         <ul>
             {nameList}
